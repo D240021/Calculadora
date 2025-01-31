4533db59fffe78e19235b686282b271d5fcfae62
IannLS-2001 <iannlopez15@gmail.com>
2025-01-31 17:04:03 -0600

    Practica tetz

--- Logica/main.js
+++ Logica/main.js
@@ -1,42 +1,42 @@
 
-const OPERADORES_POSIBLES = new Set(['+', '/', '*','-']);
+const OPERADORES_POSIBLES = new Set(['+', '/', '*', '-']);
+const CAMPO_TRABAJO = document.getElementById("campoTrabajo");
 
-function sumar(){
 
-    let campoTrabajo = document.getElementById("campoTrabajo");
-    campoTrabajo.textContent = 10+1;
-
-}
 
 function mostrarEnCampoTrabajo(elementoHTML) {
-    let campoTrabajo = document.getElementById("campoTrabajo");
     let operadoresInicialesProhibidos = new Set(['+', '/', '*']);
     let contenidoElementoHTML = elementoHTML.textContent;
 
     if (this.esCampoTrabajoVacio() && !operadoresInicialesProhibidos.has(contenidoElementoHTML)) {
-        campoTrabajo.textContent = contenidoElementoHTML;
+        CAMPO_TRABAJO.textContent = contenidoElementoHTML;
         return;
     }
 
-    if(existeSimboloAlFinal(campoTrabajo.textContent) && OPERADORES_POSIBLES.has(contenidoElementoHTML)){
+    if (existeSimboloAlFinal(CAMPO_TRABAJO.textContent) && OPERADORES_POSIBLES.has(contenidoElementoHTML)) {
         return;
-    }   
-    
-    campoTrabajo.textContent += contenidoElementoHTML;
+    }
+
+    CAMPO_TRABAJO.textContent += contenidoElementoHTML;
 }
 
 
-function borrarContenido(){
-    if(!this.esCampoTrabajoVacio()){
-        let campoTrabajo = document.getElementById("campoTrabajo");
-        campoTrabajo.textContent = 0;
+function borrarContenido() {
+    if (!this.esCampoTrabajoVacio()) {
+
+        CAMPO_TRABAJO.textContent = 0;
     }
 }
 
-function esCampoTrabajoVacio(){
-    return document.getElementById("campoTrabajo").textContent == '0';
+function esCampoTrabajoVacio() {
+    return CAMPO_TRABAJO.textContent == '0';
 }
 
 function existeSimboloAlFinal(texto) {
-    return OPERADORES_POSIBLES.has(texto.slice(-1)); 
+    return OPERADORES_POSIBLES.has(texto.slice(-1));
+}
+
+function calcularResultado() {
+
+
 }
